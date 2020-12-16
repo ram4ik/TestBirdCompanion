a80f872541b5e187a1e788230db3063fb6411a8d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-16 12:44:51 +0200

    Initial Commit

---
+++ TestBirdCompanion/TestBirdCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBirdCompanionApp.swift
+//  TestBirdCompanion
+//
+//  Created by ramil on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TestBirdCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
